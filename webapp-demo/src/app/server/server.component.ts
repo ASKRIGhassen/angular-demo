9fd85a8fc4b39c59502b83d91530a97252d5adee
ASKRIGhassen <gh.askri@gmail.com>
2020-08-09 00:43:03 +0200

    gnFor & ngClass & ngStyle

--- webapp-demo/src/app/server/server.component.ts
+++ webapp-demo/src/app/server/server.component.ts
@@ -1,13 +1,28 @@
-
 import {Component} from '@angular/core';
+
 @Component({
- selector: 'app-server',
- templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [`
+    .online{
+        color: white
+      }
+    `]
 })
-export class ServerComponent{
-serverId = 10;
-serverStatus = 'offline';
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+  constructor() {
+    this.serverId = Math.random();
+    this.serverStatus = this.serverId > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus(): string {
-  return this.serverStatus;
-}
+    return this.serverStatus;
+  }
+
+  getColor(): string {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
--- webapp-demo/src/app/servers/servers.component.ts
+++ webapp-demo/src/app/servers/servers.component.ts
@@ -3,13 +3,20 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styles: [`
+    .online{
+        color: white
+      }
+    `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreatedStatus = 'No server has been created';
   serverName = 'TestServer';
   userName = '';
+  serverCreated = false;
+  servers = ['TestServer', 'DevServer'];
+  logs = [];
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +28,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(): void {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreatedStatus = 'New server has been created! ' + this.serverName;
   }
 
@@ -34,6 +43,14 @@ export class ServersComponent implements OnInit {
 
   onReset(): void {
     this.userName = '';
+  }
+  onClickDisplay(): void {
+    this.logs.push(Date.now());
+  }
+
+
+  getColor(index: number): string {
+    return index >= 4 ? 'blue' : 'green';
   }
 
 }
